9d09e8284232b19dd3e7a94c9a87aca6c52291c3
Marion Robert <mrobert148@gmail.com>
2024-06-07 14:20:35 -0400

    set up provider

--- src/store.ts
+++ src/store.ts
@@ -1,13 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
-import themeReducer from './features/theme/themeSlice';
-import cartReducer from './features/cart/cartSlice';
+// import themeReducer from './features/theme/themeSlice';
+// import cartReducer from './features/cart/cartSlice';
 import userReducer from './features/user/userSlice';
 // ...
 
 export const store = configureStore({
   reducer: {
-    themeState: themeReducer,
-    cartState: cartReducer,
+    // themeState: themeReducer,
+    // cartState: cartReducer,
     userState: userReducer,
   },
 });
