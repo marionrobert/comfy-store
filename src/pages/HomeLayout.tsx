dbcee391c2299f5afbef9d67e061d735ee2d0b32
Marion Robert <mrobert148@gmail.com>
2024-06-07 15:12:32 -0400

    invoke header and style it

--- src/pages/HomeLayout.tsx
+++ src/pages/HomeLayout.tsx
@@ -1,11 +1,14 @@
 import { Outlet } from 'react-router-dom';
+import { Header } from '@/components';
 
 const HomeLayout = () => {
   return (
     <>
-      <header>header</header>
+      <Header />
       <nav>navbar</nav>
-      <Outlet />
+      <div className='align-element py-20'>
+        <Outlet />
+      </div>
     </>
   );
 };
