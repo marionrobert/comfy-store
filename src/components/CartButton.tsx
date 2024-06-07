6b6ae12a4b0692909c5792351e361989a96f7e33
Marion Robert <mrobert148@gmail.com>
2024-06-07 16:37:19 -0400

    text-black in dark mode for number items in cart

--- src/components/CartButton.tsx
+++ src/components/CartButton.tsx
@@ -14,7 +14,7 @@ function CartButton() {
     >
       <Link to='/cart'>
         <ShoppingCart />
-        <span className='absolute -top-3 -right-3 bg-primary text-white rounded-full h-6 w-6 flex items-center justify-center text-xs'>
+        <span className='absolute -top-3 -right-3 bg-primary text-white dark:text-black rounded-full h-6 w-6 flex items-center justify-center text-xs'>
           {numItemsInCart}
         </span>
       </Link>
