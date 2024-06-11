78621deb4068073633fa8fd512bd91a0e5f09ba5
Marion Robert <mrobert148@gmail.com>
2024-06-11 11:07:51 -0400

    format price

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,3 +2,4 @@ export { applyTheme } from './applyTheme';
 export * from './links';
 export * from "./types";
 export {customFetch} from "./customFetch";
+export {formatAsDollars} from "./formatAsDollars";
