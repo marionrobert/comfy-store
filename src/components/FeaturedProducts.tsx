6d011ebd6fec476e885c5d4c2edb3c2bd960f858
Marion Robert <mrobert148@gmail.com>
2024-06-11 11:06:52 -0400

    separator component from shadcn, sectionTitle and FeaturedProducts components

--- src/components/FeaturedProducts.tsx
+++ src/components/FeaturedProducts.tsx
@@ -1,4 +1,11 @@
-const FeaturedPproducts = () => {
-  return <h3>Composant FeaturedPproducts</h3>
+import ProductsGrid from './ProductsGrid';
+import SectionTitle from './SectionTitle';
+const FeaturedProducts = () => {
+  return (
+    <section className='pt-24 '>
+      <SectionTitle text='featured products' />
+      <ProductsGrid />
+    </section>
+  );
 };
-export default FeaturedPproducts;
+export default FeaturedProducts;
--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -1,4 +1,13 @@
-const SectionTitle = () => {
-  return <h3>Composant SectionTitle</h3>
+import { Separator } from '@/components/ui/separator';
+
+const SectionTitle = ({ text }: { text: string }) => {
+  return (
+    <div>
+      <h2 className='text-3xl font-medium tracking-wider capitalize mb-8'>
+        {text}
+      </h2>
+      <Separator />
+    </div>
+  );
 };
 export default SectionTitle;
