119860332a58491f182c34962149ffe7d853898f
Marion Robert <mrobert148@gmail.com>
2024-06-12 16:31:03 -0400

    FormInput.tsx

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -1,15 +1,20 @@
 import { Form, useLoaderData } from 'react-router-dom';
 import { type ProductsResponseWithParams } from '@/utils';
+import FormInput from './FormInput';
+
 function Filters() {
   const { meta, params } = useLoaderData() as ProductsResponseWithParams;
-  const { search } = params;
+  const { search, company, category, shipping, order, price } = params;
+
   return (
-    <Form>
-      <div>
-        <Label htmlFor='search'>Search Product</Label>
-        <Input id='search' name='search' type='text' defaultValue={search} />
-      </div>
+    <Form className='border rounded-md px-8 py-4 grid gap-x-4 gap-y-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center'>
+      {/* search */}
+      <FormInput
+        type='search'
+        label='search product'
+        name='search'
+        defaultValue={search}
+      />
     </Form>
   );
 }
-export default Filters;
