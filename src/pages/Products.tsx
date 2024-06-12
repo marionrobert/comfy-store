48d8f3671a55399cb33f274c715c974385700762
Marion Robert <mrobert148@gmail.com>
2024-06-12 16:23:38 -0400

    Products Loader

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,13 +1,21 @@
 import { Filters, ProductsContainer, PaginationContainer } from '@/components';
-import { customFetch, type ProductsResponse } from '../utils';
+import { customFetch, type ProductsResponse, type ProductsResponseWithParams, } from '../utils';
 import { type LoaderFunction } from 'react-router-dom';
 
 const url = '/products';
 
-export const loader: LoaderFunction = async (): Promise<ProductsResponse> => {
-  const response = await customFetch<ProductsResponse>(url);
+export const loader: LoaderFunction = async ({
+  request,
+}): Promise<ProductsResponseWithParams> => {
+  const params = Object.fromEntries([
+    ...new URL(request.url).searchParams.entries(),
+  ]);
 
-  return { ...response.data };
+  const response = await customFetch<ProductsResponse>(url, {
+    params,
+  });
+
+  return { ...response.data, params };
 };
 
 const Products = () => {
