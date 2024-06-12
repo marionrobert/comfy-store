37d4a13afd985f58e2183a67a5a50917c6140f38
Marion Robert <mrobert148@gmail.com>
2024-06-12 16:28:28 -0400

    filters.tsx

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -1,27 +1,14 @@
-import { Form, useLoaderData, Link } from 'react-router-dom';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
-import { Button } from './ui/button';
-
+import { Form, useLoaderData } from 'react-router-dom';
+import { type ProductsResponseWithParams } from '@/utils';
 function Filters() {
+  const { meta, params } = useLoaderData() as ProductsResponseWithParams;
+  const { search } = params;
   return (
-    <Form className='border rounded-md px-8 py-4 grid gap-x-4 gap-y-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center'>
-      <div className='mb-2'>
+    <Form>
+      <div>
         <Label htmlFor='search'>Search Product</Label>
-        <Input id='search' name='search' type='text' defaultValue='' />
+        <Input id='search' name='search' type='text' defaultValue={search} />
       </div>
-      <Button type='submit' size='sm' className='self-end mb-2'>
-        search
-      </Button>
-      <Button
-        type='button'
-        asChild
-        size='sm'
-        variant='outline'
-        className='self-end mb-2'
-      >
-        <Link to='/products'>reset</Link>
-      </Button>
     </Form>
   );
 }
