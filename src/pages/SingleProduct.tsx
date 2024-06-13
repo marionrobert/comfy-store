b79b33ecce646821abb254c7a6f97130e82ef7a6
Marion Robert <mrobert148@gmail.com>
2024-06-13 17:12:16 -0400

    import and setup loader in App.tsx

--- src/pages/SingleProduct.tsx
+++ src/pages/SingleProduct.tsx
@@ -1,6 +1,22 @@
+import { useLoaderData } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import {
+  customFetch,
+  formatAsDollars,
+  type SingleProductResponse,
+} from '@/utils';
+import { useState } from 'react';
+import { Button } from '@/components/ui/button';
+import { Separator } from '@/components/ui/separator';
+
+import { type LoaderFunction } from 'react-router-dom';
+
+export const loader: LoaderFunction = async ({ params }) => {
+  console.log(params);
+  return null;
+};
+
 const SingleProduct = () => {
-  return (
-    <></>
-  )
-}
+  return <h1 className='text-4xl'>SingleProduct Page</h1>;
+};
 export default SingleProduct;
