81f282e18dd07bb9477eba41b96ea39cc5dc0591
Marion Robert <mrobert148@gmail.com>
2024-06-13 18:21:01 -0400

    CartItemsList

--- src/components/CartItemsList.tsx
+++ src/components/CartItemsList.tsx
@@ -1,4 +1,31 @@
+import { useAppSelector } from '@/hooks';
+import { Card } from './ui/card';
+
+import {
+  FirstColumn,
+  SecondColumn,
+  ThirdColumn,
+  FourthColumn,
+} from './CartItemColumns';
 const CartItemsList = () => {
-  return <h1>CartItemsList</h1>
-}
+  const cartItems = useAppSelector((state) => state.cartState.cartItems);
+
+  return (
+    <div>
+      {cartItems.map((cartItem) => {
+        const { cartID, title, price, image, amount, company, productColor } =
+          cartItem;
+        return (
+          <Card
+            key={cartID}
+            className='flex flex-col gap-y-4 sm:flex-row flex-wrap p-6 mb-8'
+          >
+            Cart Item
+          </Card>
+        );
+      })}
+    </div>
+  );
+};
+
 export default CartItemsList;
