1e21528fc7fdcc8e0ff2f18b48d6dbd51c14da8e
Marion Robert <mrobert148@gmail.com>
2024-06-13 18:15:21 -0400

    SingleProduct - AddItem

--- src/store.ts
+++ src/store.ts
@@ -1,13 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import themeReducer from './features/theme/themeSlice';
-// import cartReducer from './features/cart/cartSlice';
+import cartReducer from './features/cart/cartSlice';
 import userReducer from './features/user/userSlice';
 // ...
 
 export const store = configureStore({
   reducer: {
     themeState: themeReducer,
-    // cartState: cartReducer,
+    cartState: cartReducer,
     userState: userReducer,
   },
 });
