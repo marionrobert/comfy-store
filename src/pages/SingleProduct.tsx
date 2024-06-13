3c0754bdb11fe7a9b66884f8eafa8c536b41bc92
Marion Robert <mrobert148@gmail.com>
2024-06-13 17:12:52 -0400

    SingleProduct - Load Products

--- src/pages/SingleProduct.tsx
+++ src/pages/SingleProduct.tsx
@@ -11,12 +11,28 @@ import { Separator } from '@/components/ui/separator';
 
 import { type LoaderFunction } from 'react-router-dom';
 
-export const loader: LoaderFunction = async ({ params }) => {
-  console.log(params);
-  return null;
+export const loader: LoaderFunction = async ({
+  params,
+}): Promise<SingleProductResponse> => {
+  const response = await customFetch<SingleProductResponse>(
+    `/products/${params.id}`
+  );
+
+  return { ...response.data };
 };
 
 const SingleProduct = () => {
+  const { data: product } = useLoaderData() as SingleProductResponse;
+  const { image, title, price, description, colors, company } =
+    product.attributes;
+  const dollarsAmount = formatAsDollars(price);
+  const [productColor, setProductColor] = useState(colors[0]);
+  const [amount, setAmount] = useState(1);
+
+  const addToCart = () => {
+    consol.log('add to cart');
+  };
+
   return <h1 className='text-4xl'>SingleProduct Page</h1>;
 };
 export default SingleProduct;
