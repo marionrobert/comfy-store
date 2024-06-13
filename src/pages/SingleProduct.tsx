2abf8992188db4ca106276a43ae9da78391cf0b4
Marion Robert <mrobert148@gmail.com>
2024-06-13 17:22:51 -0400

    SingleProduct - Initial Return

--- src/pages/SingleProduct.tsx
+++ src/pages/SingleProduct.tsx
@@ -30,9 +30,56 @@ const SingleProduct = () => {
   const [amount, setAmount] = useState(1);
 
   const addToCart = () => {
-    consol.log('add to cart');
+    console.log('add to cart');
   };
 
-  return <h1 className='text-4xl'>SingleProduct Page</h1>;
+  return (
+    <section>
+      <div className='flex gap-x-2 h-6 items-center'>
+        <Button asChild variant='link' size='sm'>
+          <Link to='/'>Home</Link>
+        </Button>
+        <Separator orientation='vertical' />
+        <Button asChild variant='link' size='sm'>
+          <Link to='/products'>Products</Link>
+        </Button>
+      </div>
+      {/* PRODUCT */}
+      <div className='mt-6 grid gap-y-8 lg:grid-cols-2 lg:gap-x-16'>
+        {/* IMAGE FIRST COL */}
+        <img
+          src={image}
+          alt={title}
+          className='w-96 h-96 object-cover rounded-lg lg:w-full'
+        />
+        {/* PRODUCT INFO SECOND COL */}
+        <div>
+          <h1 className='capitalize text-3xl font-bold'>{title}</h1>
+          <h4 className='text-xl mt-2'>{company}</h4>
+          <p className='mt-3 text-md bg-muted inline-block p-2 rounded-md'>
+            {dollarsAmount}
+          </p>
+          <p className='mt-6 leading-8'>{description}</p>
+          {/* COLORS  */}
+          <SelectProductColor
+            colors={colors}
+            productColor={productColor}
+            setProductColor={setProductColor}
+          />
+
+          {/* AMOUNT  */}
+          <SelectProductAmount
+            mode={Mode.SingleProduct}
+            amount={amount}
+            setAmount={setAmount}
+          />
+          {/* CART BUTTON  */}
+          <Button size='lg' className='mt-10' onClick={addToCart}>
+            Add to bag
+          </Button>
+        </div>
+      </div>
+    </section>
+  );
 };
 export default SingleProduct;
