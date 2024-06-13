2dd688ed652456dedadae975899af24b9cf08526
Marion Robert <mrobert148@gmail.com>
2024-06-13 18:19:39 -0400

    create CartTotals, CartItemsList, CartItemColumns and import/export them

---
+++ src/components/CartItemColumns.tsx
@@ -0,0 +1,4 @@
+const CartItemColumns = () => {
+  return <h1>CartItemColumns</h1>
+}
+export default CartItemColumns;
---
+++ src/components/CartTotals.tsx
@@ -0,0 +1,4 @@
+const CartTotals = () => {
+  return <h1>CartTotals</h1>
+}
+export default CartTotals;
