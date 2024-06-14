c3f494cb94a3581b7fd3066e059334ef3670c556
Marion Robert <mrobert148@gmail.com>
2024-06-14 09:49:11 -0400

    register page - setup

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,6 +1,42 @@
-const Register = () => {
+import { ActionFunction, Form, Link, redirect } from 'react-router-dom';
+import { Card, CardHeader, CardContent, CardTitle } from '@/components/ui/card';
+import { Button } from '@/components/ui/button';
+import { SubmitBtn, FormInput } from '@/components';
+import { customFetch } from '@/utils';
+import { toast } from '@/components/ui/use-toast';
+import { AxiosError } from 'axios';
+
+export const action: ActionFunction = async ({ request }): Promise<null> => {
+  return null;
+};
+
+function Register() {
   return (
-    <></>
-  )
+    <section className='h-screen grid place-items-center'>
+      <Card className='w-96 bg-muted'>
+        <CardHeader>
+          <CardTitle className='text-center'>Register</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Form>
+            <FormInput type='text' name='username' defaultValue='test' />
+            <FormInput type='email' name='email' defaultValue='test@test.com' />
+            <FormInput type='password' name='password' defaultValue='secret' />
+
+            <Button type='submit' variant='default' className='w-full mt-4'>
+              Submit
+            </Button>
+
+            <p className='text-center mt-4'>
+              Already a member?
+              <Button type='button' asChild variant='link'>
+                <Link to='/login'>Login</Link>
+              </Button>
+            </p>
+          </Form>
+        </CardContent>
+      </Card>
+    </section>
+  );
 }
 export default Register;
