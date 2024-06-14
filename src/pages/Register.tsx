f96f1ed7ad868dce15eca277cad374c9b1280822
Marion Robert <mrobert148@gmail.com>
2024-06-14 09:56:29 -0400

    register user

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -6,11 +6,25 @@ import { customFetch } from '@/utils';
 import { toast } from '@/components/ui/use-toast';
 import { AxiosError } from 'axios';
 
-export const action: ActionFunction = async ({ request }): Promise<null> => {
+export const action: ActionFunction = async ({
+  request,
+}): Promise<Response | null> => {
   const formData = await request.formData();
   const data = Object.fromEntries(formData);
-  console.log(data);
-  return null;
+  try {
+    await customFetch.post('/auth/local/register', data);
+    toast({ description: 'Registered' });
+    return redirect('/login');
+  } catch (error) {
+    // console.log(error);
+    const errorMsg =
+      error instanceof AxiosError
+        ? error.response?.data.error.message
+        : 'Registration Failed';
+    toast({ description: errorMsg });
+
+    return null;
+  }
 };
 
 function Register() {
